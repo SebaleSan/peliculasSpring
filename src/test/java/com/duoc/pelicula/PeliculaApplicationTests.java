56e6ae5d93af4c81cf76ffaaedb133cf232d601b
SebaleSan <se.santanderv@duocuc.cl>
2026-03-22 19:35:57 -0300

    Primer commit

---
+++ src/test/java/com/duoc/pelicula/PeliculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.pelicula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
